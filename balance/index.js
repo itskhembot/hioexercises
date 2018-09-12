05a94ec6b7449fde855573a0ed925c8952d34816
itskhembot <itskhembot@gmail.com>
2018-09-12 15:21:19 +0800

    feature(type defs)

--- balance/index.js
+++ balance/index.js
@@ -1,35 +1,64 @@
-const { apolloserver, gql } = require('apollo-server');
+const { ApolloServer, gql } = require('apollo-server');
 
 // This is a (sample) collection of books we'll be able to query
 // the GraphQL server for.  A more complete example might fetch
 // from an existing data source like a REST API or database.
-const books = [
+const accounts = [
   {
-    title: 'Harry Potter and the Chamber of Secrets',
-    author: 'J.K. Rowling',
+    id:1,
+	balance:200,
+	availablebalance:100,
   },
   {
-    title: 'Jurassic Park',
-    author: 'Michael Crichton',
+    id:2,
+	balance:100,
+	availablebalance:150,
   },
 ];
-
+const reservedbalance = [
+  {
+    id:1,
+	context:"test balance for id 1",
+	balance:100,
+  },
+];
+const virtualbalance = [
+  {
+    id:1,
+	context:"allocate budget for id 2",
+	balance:50,
+  },
+];
 // Type definitions define the "shape" of your data and specify
 // which ways the data can be fetched from the GraphQL server.
-//use the schema.gql
 const typeDefs = gql`
   # Comments in GraphQL are defined with the hash (#) symbol.
 
   # This "Book" type can be used in other type declarations.
-  type Book {
-    title: String
-    author: String
-  }
+type Account {
+  id: ID!
+  balance: Float!
+  availableBalance(context: String): Float!
+}
+
+type ReservedBalance {
+  id: ID!
+  context: String!
+  balance: Float!
+}
+
+type VirtualBalance {
+  id: ID!
+  context: String!
+  balance: Float!
+}
 
   # The "Query" type is the root of all GraphQL queries.
   # (A "Mutation" type will be covered later on.)
   type Query {
-    books: [Book]
+    accounts: [Account]
+	reservedbalance: [ReservedBalance]
+	virtualbalance: [VirtualBalance]
   }
 `;
 
@@ -37,17 +66,19 @@ const typeDefs = gql`
 // schema.  We'll retrieve books from the "books" array above.
 const resolvers = {
   Query: {
-    books: () => books,
+    accounts: () => accounts,
+    reservedbalance: () => reservedbalance,
+    virtualbalance: () => virtualbalance,
   },
 };
 
 // In the most basic sense, the ApolloServer can be started
 // by passing type definitions (typeDefs) and the resolvers
 // responsible for fetching the data for those types.
-const server = new apolloserver({ typeDefs, resolvers });
+const server = new ApolloServer({ typeDefs, resolvers });
 
 // This `listen` method launches a web-server.  Existing apps
 // can utilize middleware options, which we'll discuss later.
 server.listen().then(({ url }) => {
-  console.log(`🚀  Server ready at ${url}`);
+  console.log(`??  Server ready at ${url}`);
 });
