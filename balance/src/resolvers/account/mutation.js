1e71358cdaabf873350e21f577a0f60a37b40010
itskhembot <itskhembot@gmail.com>
2018-09-18 13:25:33 +0800

    update(mutation):updateBalance mutation test

--- balance/src/resolvers/account/mutation.js
+++ balance/src/resolvers/account/mutation.js
@@ -1,24 +1,35 @@
 import RequestModel from '../../models/request';
-
+import AccountModel from '../../models/account';
 export default {
   Mutation: {
     updateBalance: async (obj, args) => {
-      const request = RequestModel.findOrCreate({
+      let request = await RequestModel.findOne({
         where: {
           id: args.request
-        },
-        defaults: {//object containing fields and values to apply
-          id: args.request,
-          
-        },
+        }
       });
-      //findOne if found
-      //return request.result;
-      //else
-      //create request
-      //updatebalance
-      //return amount
+      if (!request) {
+        const account = await AccountModel.findOne({ where: { id: args.account } })
+          .on('success', function (AccountModel) {
+            if (AccountModel) {
+              AccountModel.updateAttributes({
+                balance: args.amount
+              })}
+          });
+          let request = await RequestModel.build({
+            id: args.request,
+            result: args.amount
+          });
+          return request.result;
+        }
 
-    },
+            //findOne if found
+            //return request.result;
+            //else
+            //updatebalance
+            //create request
+            //return result
+      return request.result;
+     },
   },
 };
