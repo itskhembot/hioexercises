ba2ca2c57c1a9c936d10215e011d176c93e35284
itskhembot <itskhembot@gmail.com>
2018-09-25 08:09:42 +0800

    update(balance):account resource

--- balance/src/resource/account.js
+++ balance/src/resource/account.js
@@ -2,14 +2,18 @@ import AccountModel from '../models/account';
 
 export default {
   updateBalanceTable: async (obj, args) => {
-    await AccountModel.findOne({ where: { id: args.account } })
-      .on('success', (result) => {
-        if (result) {
-          AccountModel.updateAttributes({
-            balance: args.amount,
-          });
-        }
-        return args.amount;
-      });
+    const account = await AccountModel.findOne({ where: { id: args.account } });
+    let val;
+    try {
+      if (account) {
+        AccountModel.updateAttributes({
+          balance: args.amount,
+        });
+        val = args.amount;
+      }
+    } catch (err) {
+      val = err.message;
+    }
+    return val;
   },
 };
